ba12d8cc766f93eec70ee131dbb425417fc934a2
J.D. Sandifer <sandifer.jd@gmail.com>
2018-12-16 16:22:31 -0800

    Remove old code and simplify

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@ class Quotes {
   constructor(apiCommand) {
     this.quoteApiRequest = apiCommand
     this.quotes = []
-    let newQuotes = this.getMoreQuotes()
-    this.quotes = this.quotes.concat(newQuotes)
-  } 
+    this.getMoreQuotes()
+    console.log("const", this.quotes)
+  }
   
   getMoreQuotes() {
-    const newQuotesPromise = $.ajax( {
-      url: this.quoteApiRequest,
-      cache: false
-      } )
-    const newQuotes = newQuotesPromise.then(quoteData => {
+    const newQuotesPromise = $.ajax({
+        url: this.quoteApiRequest,
+        cache: false
+      })
+    newQuotesPromise.then(quoteData => {
       let newQuotes = []
       quoteData.forEach( quote => {
         let nextQuote = {}
@@ -21,40 +21,36 @@ class Quotes {
       })
       this.quotes = this.quotes.concat(newQuotes)
     })
-    return newQuotes
   }
   
   newQuote() {
-    if (this.quotes.length < 5) {
-      let newQuotes = this.getMoreQuotes()
-      this.quotes = this.quotes.concat(newQuotes)
+    if (this.quotes.length <= 5) {
+      this.getMoreQuotes()
     }
     const newQuote = this.quotes.shift()
     return newQuote
   }
 }
 
-// Activates all the pieces
-// of showing a new quote
 const displayNewQuote = () => {
   const newQuote = quotes.newQuote()
   displayQuote( newQuote )
   updateLink( newQuote )
 }
 
-// Displays given quote and author
 const displayQuote = ( quote ) => {
   $( "#quote" ).html( quote.text )
   $( "#author" ).html( "- " + quote.author )
 }
 
-// Updates link with given quote and author
 const updateLink = ( quote ) => {
-  const linkBase = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="
+  const linkBase = 
+      "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="
   let newLink = linkBase + encodeURI( quote.text + ' ' + quote.author )
   $( "#twitter-link" ).attr( "href", newLink )
 }
 
+// Initialize class with URL for request for 10 random quotes.
 const quotes = new Quotes("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=10")
 
 $('#new-quote').click( () => { displayNewQuote() } )
